0fa67ccfee794650c5cb5cc620a4b8fcb4a43e23
Juliet Wambuku <juliet@buupass.com>
2025-05-27 12:26:01 +0300

    Initial commit - Project scaffold

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from '@/components/Card'
-import Button from '@/components/common/Button'
+import Button from '@/components/Button'
 
 const Landing: React.FC = () => {
     return (
